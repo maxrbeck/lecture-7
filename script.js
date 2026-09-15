30cc0887bc85e5aeff5ce7d83d92c4759d658326
Max <beckmr41@gmail.com>
2026-09-15 10:55:27 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function double(number) {
+  return number * 2;
+}
+
+const myNumber = 4;
+const myString = "John";
+
+console.log(typeof double);
+
+double = function (number) {
+  return number + number;
+};
